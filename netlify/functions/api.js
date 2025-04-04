c46fcfc4db9f14aa2cfc0575ed592a358fc3c50b
mehul-syncsignature <mehul.syncsignature@gmail.com>
2025-04-04 13:38:03 +0530

    Add missing dependencies for Netlify functions

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,18 +1,15 @@
 const express = require('express')
-const payload = require('payload')
 const serverless = require('serverless-http')
+const payload = require('payload')
 
 const app = express()
 
 // Initialize Payload
 payload.init({
   secret: process.env.PAYLOAD_SECRET,
-  // If you're using PostgreSQL
   postgresConfig: {
     url: process.env.DATABASE_URI,
   },
-  // Or for MongoDB
-  // mongoURL: process.env.MONGODB_URI,
   express: app,
 })
 
